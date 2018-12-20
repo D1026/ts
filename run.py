6df1a32d169ffdf34eab3e251ec2a782bb83a5d4
ivan_duan <492987559@qq.com>
2018-12-20 11:46:44 +0800

    1

--- run.py
+++ run.py
@@ -6,15 +6,32 @@ import matplotlib.pyplot as plt
 
 with open('vm_cpu.pkl', mode='rb') as f:
     data = pickle.load(f)
+
+cputs_Dic = {}
+print(data.keys())
+print(type(data['FusionCompute.m3_urn:sites:43F10830:vms:i-0000002B']['COLL_TIME']))
 for k in data.keys():
     print(data[k]['COLL_TIME'][0], type(data[k]['COLL_TIME'][0]))
     print(data[k]['KPI_VALUE'][0], type(data[k]['KPI_VALUE'][0]))
     idx = pd.DatetimeIndex(data[k]['COLL_TIME'])
-    print(idx)
+    # print(idx)
     ts = pd.Series(np.array(data[k]['KPI_VALUE']), index=idx)
-    ts.plot(kind='line', figsize=(32, 10))
-    plt.show()
-    plt.savefig('123.png')
-    print(type(ts))
 
-    exit(888)
+    cputs_Dic[k] = ts
+    # ---------- plot ----------
+    # ts.plot(kind='line', figsize=(32, 10))
+    # plt.show()
+    # plt.savefig('123.png')
+    # print(type(ts))
+with open('vm_cputs.pkl', mode='wb') as f:
+    pickle.dump(cputs_Dic, f)
+exit(888)
+
+for k in data.keys():
+    vm_ts = data[k]
+    print(vm_ts.index)
+    vm_ts = vm_ts.resample('6min')
+    if 'NaN' in vm_ts:
+        print('存在插值')
+    vm_ts = vm_ts.ffill()
+    exit(666)
