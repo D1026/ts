0b858c473af3864e49e275a67d66f0b61b94612b
ivan_duan <492987559@qq.com>
2018-12-20 17:12:02 +0800

    0

---
+++ freeCheck.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+# df = pd.DataFrame(np.arange(14).reshape([7, 2]), index=[11, 12, 13, 14, 15, 16, 17], columns=['A', 'B'])
+# df1 = pd.DataFrame(df[df['B']<0])
+# print(df)
+#
+# # print(df1['B'].index[1])
+# print(('A' in df))
+# print((17 in df['A']))  # X in DataFrame: return x in dataframe.column,    X in Series: return x in series.index
+
+# -----------------------------------------------
+# rng = pd.date_range('1/1/2011', periods=72, freq='H')
+# ts = pd.Series(np.random.randn(len(rng)), index=rng)
+# print(type(ts.index))
+# converted = ts.asfreq('45Min', method='pad')
+# print(converted.head())
+
+# ------------------------------------------------
+se = pd.Series(np.arange(10), index=[1, 2, 3, 4, 4, 5, 6, 7, 8, 9])
+print(len(se))
+print(se[4])
+print(len(se[4]))
+print(pd.Series)
--- run.py
+++ run.py
@@ -9,10 +9,15 @@ with open('vm_cpu.pkl', mode='rb') as f:
 
 cputs_Dic = {}
 print(data.keys())
-print(type(data['FusionCompute.m3_urn:sites:43F10830:vms:i-0000002B']['COLL_TIME']))
+
+# print(type(data['FusionCompute.m3_urn:sites:43F10830:vms:i-0000002B']['COLL_TIME']))
+# print(data['FusionCompute.m3_urn:sites:43F10830:vms:i-0000002B']['COLL_TIME'])
+# print(data['FusionCompute.m3_urn:sites:43F10830:vms:i-0000002B']['COLL_TIME'][7064])
 for k in data.keys():
-    print(data[k]['COLL_TIME'][0], type(data[k]['COLL_TIME'][0]))
-    print(data[k]['KPI_VALUE'][0], type(data[k]['KPI_VALUE'][0]))
+    print(k)
+    # print(data[k]['COLL_TIME'][0], type(data[k]['COLL_TIME'][0])) # 指定的index为整数，则自然数索引失效
+    # print(data[k]['COLL_TIME'][data[k]['COLL_TIME'].index[0]], type(data[k]['COLL_TIME'][data[k]['COLL_TIME'].index[0]]))
+    # print(data[k]['KPI_VALUE'][data[k]['KPI_VALUE'].index[0]], type(data[k]['KPI_VALUE'][data[k]['KPI_VALUE'].index[0]]))
     idx = pd.DatetimeIndex(data[k]['COLL_TIME'])
     # print(idx)
     ts = pd.Series(np.array(data[k]['KPI_VALUE']), index=idx)
@@ -25,13 +30,27 @@ for k in data.keys():
     # print(type(ts))
 with open('vm_cputs.pkl', mode='wb') as f:
     pickle.dump(cputs_Dic, f)
-exit(888)
 
-for k in data.keys():
-    vm_ts = data[k]
-    print(vm_ts.index)
-    vm_ts = vm_ts.resample('6min')
-    if 'NaN' in vm_ts:
-        print('存在插值')
-    vm_ts = vm_ts.ffill()
-    exit(666)
+
+# for k in data.keys():
+#     vm_ts = data[k]
+#     print(vm_ts.index)
+#     vm_ts = vm_ts.resample('6min')
+#     if 'NaN' in vm_ts:
+#         print('存在插值')
+#     vm_ts = vm_ts.ffill()
+#     exit(666)
+
+for k in cputs_Dic.keys():
+    vm_ts = cputs_Dic[k]
+    print(len(vm_ts))
+    print(len(vm_ts.index.unique()))    # <class 'pandas.core.indexes.datetimes.DatetimeIndex'>
+    for i in vm_ts.index:
+        if isinstance(vm_ts[i], pd.Series):
+            print(vm_ts[i])
+    exit(777)
+    converted = vm_ts.asfreq('45Min', method='pad')
+    print(converted)
+    # print(vm_ts.isnull())
+    # vm_ts = vm_ts.ffill()
+    # print(vm_ts.isnull())
