e02e083a8e06c70f151d02127d77085667e1aaf2
ivan_duan <492987559@qq.com>
2018-12-25 15:15:53 +0800

    1

--- preproc.py
+++ preproc.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import train_test_split
 with open('vm_cpuDataNormal.pkl', mode='rb') as f:
     all = pickle.load(f)
 
-for k in all.key():
+for k in all.keys():
+    print(k)
     data = all[k]
     xs = []
     ys = []
@@ -14,5 +15,5 @@ for k in all.key():
 
     print(k+'：', len(xs))
     train_x, test_x, train_y, test_y = train_test_split(xs, ys, test_size=0.2, random_state=42)
-    with open('data/'+k, mode='wb') as f:
+    with open('data/'+k[-2:], mode='wb') as f:
         pickle.dump((train_x, test_x, train_y, test_y), f)
